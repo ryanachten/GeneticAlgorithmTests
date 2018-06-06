40f3edfcf083940043fb4b5979f0604bf53e923b
Ryan Achten <ryanachten@gmail.com>
2018-06-06 21:41:58 +1200

    Add init seeking agent algorithm and fbx model loading

---
+++ js/main.js
@@ -0,0 +1,162 @@
+if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
+
+var container, stats, controls;
+var camera, scene, renderer, light;
+
+var clock = new THREE.Clock();
+
+var mixers = [];
+
+let vehicle;
+let object;
+let target;
+const groundSize = 2000;
+
+init();
+animate();
+
+function init() {
+
+  // Vehicle driver for GA
+  vehicle = new Vehicle(0, -800);
+
+
+  container = document.createElement( 'div' );
+  document.body.appendChild( container );
+
+  camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
+  camera.position.set( 100, 200, 300 );
+
+  controls = new THREE.OrbitControls( camera );
+  controls.target.set( 0, 100, 0 );
+  controls.update();
+
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color( 0xa0a0a0 );
+  // scene.fog = new THREE.Fog( 0xa0a0a0, 200, 1000 );
+
+  light = new THREE.HemisphereLight( 0xffffff, 0x444444 );
+  light.position.set( 0, 200, 0 );
+  scene.add( light );
+
+  light = new THREE.DirectionalLight( 0xffffff );
+  light.position.set( 0, 200, 100 );
+  light.castShadow = true;
+  light.shadow.camera.top = 180;
+  light.shadow.camera.bottom = -100;
+  light.shadow.camera.left = -120;
+  light.shadow.camera.right = 120;
+  scene.add( light );
+
+  // scene.add( new THREE.CameraHelper( light.shadow.camera ) );
+
+  // ground
+  var mesh = new THREE.Mesh(
+    new THREE.PlaneBufferGeometry( groundSize, groundSize ),
+    new THREE.MeshPhongMaterial( { color: 0x999999, depthWrite: false } )
+  );
+  mesh.rotation.x = - Math.PI / 2;
+  mesh.receiveShadow = true;
+  scene.add( mesh );
+
+  var grid = new THREE.GridHelper( groundSize, 20, 0x000000, 0x000000 );
+  grid.material.opacity = 0.2;
+  grid.material.transparent = true;
+  scene.add( grid );
+
+  // setup target for GE to find
+  target = new THREE.Mesh( new THREE.SphereGeometry( 5, 32, 32 ), new THREE.MeshPhongMaterial( { color: 0x000000 } ));
+  target.position.set(-500, 0, 0);
+  scene.add(target);
+  setInterval(function () {
+    target.position.set(
+      Math.random()*(groundSize/2),
+      0,
+      Math.random()*(groundSize/2),
+    );
+  }, 2000);
+
+  // model
+  var loader = new THREE.FBXLoader();
+  loader.load( 'models/Walking.fbx', function ( model ) {
+
+    object = model;
+
+    object.position.set(0, 0, groundSize*-1); //set near the edge of the ground
+
+    object.mixer = new THREE.AnimationMixer( object );
+    mixers.push( object.mixer );
+
+    var action = object.mixer.clipAction( object.animations[ 0 ] );
+    action.play();
+
+    object.traverse( function ( child ) {
+
+      if ( child.isMesh ) {
+
+        child.castShadow = true;
+        child.receiveShadow = true;
+
+      }
+
+    } );
+
+    scene.add( object );
+
+  } );
+
+  renderer = new THREE.WebGLRenderer( { antialias: true } );
+  renderer.setPixelRatio( window.devicePixelRatio );
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.shadowMap.enabled = true;
+  container.appendChild( renderer.domElement );
+
+  window.addEventListener( 'resize', onWindowResize, false );
+
+  // stats
+  stats = new Stats();
+  container.appendChild( stats.dom );
+
+}
+
+function onWindowResize() {
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+//
+
+function animate() {
+
+  requestAnimationFrame( animate );
+
+  if ( mixers.length > 0 ) {
+    for ( var i = 0; i < mixers.length; i ++ ) {
+      mixers[ i ].update( clock.getDelta() );
+    }
+  }
+
+  const xDistance = Math.abs(vehicle.position.x - target.position.x);
+  const zDistance = Math.abs(vehicle.position.y - target.position.z);
+
+
+  if (object //Make sure object has loaded
+      // Only move if the object is within distance
+      && xDistance > 1 && zDistance > 1
+    ) {
+      vehicle.seek(new THREE.Vector2(target.position.x, target.position.z));
+      vehicle.update();
+      object.position.set(vehicle.position.x, 0, vehicle.position.y);
+      object.lookAt(target.position);
+  }
+
+
+  renderer.render( scene, camera );
+
+  stats.update();
+
+}
---
+++ js/vehicle.js
@@ -0,0 +1,43 @@
+// Based on Shiffman's class: https://github.com/shiffman/The-Nature-of-Code-Examples-p5.js/blob/master/chp06_agents/NOC_6_01_Seek/vehicle.js
+
+class Vehicle {
+  constructor(x, y) {
+
+    this.acceleration = new THREE.Vector2( 0, 0 );
+    this.velocity = new THREE.Vector2( 0, 1 );
+    this.position = new THREE.Vector2( x, y );
+    this.maxSpeed = 2;
+    this.maxForce = 0.2;
+  }
+
+  // Method to update vehicle location
+  update(){
+    // update velocity
+    this.velocity.add(this.acceleration);
+    // limit speed
+    this.velocity.clampLength(0, this.maxSpeed); //instead of p5's .limit()?
+    // update position
+    this.position.add(this.velocity);
+    // reset acceleration to 0 ea. cycle
+    this.acceleration.multiplyScalar(0);
+  }
+
+  applyForce(force){
+    // Can add mass here for A = F / M
+    this.acceleration.add(force);
+  }
+
+  // Calculate steering force towards target
+  seek(target){
+    let desired = target.sub(this.position);
+    // scale to maximum speed - *p5* desired.setMag(this.maxspeed);
+      // .setMag() - Set the magnitude of this vector to the value
+    desired.normalize();
+    desired.multiplyScalar(this.maxSpeed);
+    // steering = desired minus velocity
+    const steer = desired.sub(this.velocity);
+    steer.clampLength(0, this.maxSpeed);
+
+    this.applyForce(steer);
+  }
+}
